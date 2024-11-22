222d6078e3809c52d5a3365200da035d619ead96
Jerin8246 <jerinjoseph2468@gmail.com>
2024-11-21 21:43:40 -0700

    create table for student and student_infromation

--- Server/database.sql
+++ Server/database.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE student2;
+CREATE DATABASE studentcat;
 
 
 CREATE TABLE student (
-    studentID SERIAL PRIMARY KEY,
+    studentID INTEGER PRIMARY KEY,
     dateOfBirth DATE,
     firstName VARCHAR(100),
     lastName VARCHAR(100),
@@ -10,4 +10,13 @@ CREATE TABLE student (
     address VARCHAR(255),
     emergencyContact VARCHAR(100),
     email VARCHAR(100)
+);
+
+CREATE TABLE Student_Information (
+    studentID INTEGER PRIMARY KEY,
+    major VARCHAR(100),
+    isEnrolled BOOLEAN,
+    gpa DECIMAL(3, 2),
+    enrollmentYear INTEGER,
+    FOREIGN KEY (studentID) REFERENCES Student(studentID)
 );
